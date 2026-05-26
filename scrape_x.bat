d046a5cc17663b4c40e168528ce0c9de0caff7d0
zervalsz <wenfan9527@gmail.com>
2026-05-25 22:48:14 -0700

    chore: run full pipeline after X scrape

--- scrape_x.bat
+++ scrape_x.bat
@@ -7,5 +7,9 @@ echo Embedding new posts...
 wsl bash -lc "cd /home/wf/projects/x-trend-analyzer && /home/wf/anaconda3/bin/python -m ml_pipeline.embedder"
 
 echo.
+echo Running ML pipeline (cluster / link / summarize / score)...
+wsl bash -lc "cd /home/wf/projects/x-trend-analyzer && /home/wf/anaconda3/bin/python run_pipeline.py --days 3 --skip-reset"
+
+echo.
 echo Done! Press any key to close.
 pause
